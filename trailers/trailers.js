34ab55ce50e70e7850e2732dd85e335494abe728
Dan Ayettey <55326982+Dan-Ayettey@users.noreply.github.com>
2021-02-12 09:37:40 +0100

    update

--- trailers/trailers.js
+++ trailers/trailers.js
@@ -42,6 +42,8 @@ const fetchTrailerUrls=async (iMDBData)=> {
        the .env file in the root directory should be use.
        fetch data  from themoviedb endpoint and filter for chunk of trailer id block for url data using non blocking state request
        and in turn return chunks of data
+
+       
       */
 
     const results = await fetchAll(iMDBData.map(async (find) =>
